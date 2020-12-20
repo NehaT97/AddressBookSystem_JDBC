78267106dd379fee42c769f4592d349482a90abb
NehaT97 <nehathakur764@gmail.com>
2020-12-20 17:48:28 +0530

    Welcome To AddressBook System Using JDBC

---
+++ src/main/java/com/BridgeLabZ/AddressBookSystem/Application.java
@@ -0,0 +1,8 @@
+package com.BridgeLabZ.AddressBookSystem;
+
+public class Application {
+    public static void main(String[] args){
+
+        System.out.println("Welcome to Address Book Management ");
+    }
+}
